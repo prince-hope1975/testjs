f76f86e7a849a6d5dad5c4bfbb400b3987b79615
Prince <amachreeprince7@gmail.com>
2023-06-28 19:31:09 +0100

    finally

--- src/start.js
+++ src/start.js
@@ -210,16 +210,16 @@ export const RecursiveCheck = async () => {
 };
 // const APY = 10 / 365 / 24;
 let cnt = 0;
-// schedule("*/4 * * * *", () => {
-//   console.log("Starting Cron Job", cnt);
-//   cnt++;
-//   RecursiveCheck()
-//     .then(() => {
-//       console.log({ res: "success" });
-//       console.log("Finishing Cron Job");
-//     })
-//     .catch(console.error);
-// });
+schedule("*/4 * * * *", () => {
+    console.log("Starting Cron Job", cnt);
+    cnt++;
+    RecursiveCheck()
+        .then(() => {
+        console.log({ res: "success" });
+        console.log("Finishing Cron Job");
+    })
+        .catch(console.error);
+});
 // RecursiveCheck()
 //   .then(() => {
 //     console.log({ res: "success" });
@@ -239,15 +239,15 @@ let cnt = 0;
  *
  * !MAIN cron job
  */
-schedule(`0 */2 * * *`, async () => {
-    console.log("Starting Cron Job", cnt);
-    cnt++;
-    await RecursiveCheck()
-        .then(() => {
-        console.log("Finishing Cron Job");
-    })
-        .catch(console.error);
-});
+// schedule(`0 */2 * * *`, async () => {
+//   console.log("Starting Cron Job", cnt);
+//   cnt++;
+//   await RecursiveCheck()
+//     .then(() => {
+//       console.log("Finishing Cron Job");
+//     })
+//     .catch(console.error);
+// });
 /**
  * !MAIN cron job
  */
