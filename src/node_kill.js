db170a7a8da2abf5f7bdd2ccbef27c1af05e7698
Prince <amachreeprince7@gmail.com>
2023-10-27 22:41:49 +0100

    akbeg

--- src/node_kill.js
+++ src/node_kill.js
@@ -1,37 +1,32 @@
 import { schedule } from "node-cron";
-import { exec as _exec } from "child_process";
-import util from "node:util";
-const exec = util.promisify(_exec); // Define// import {} from "";
+import { exec } from "child_process";
 // Define the process name (you can change this if you're looking for Chromium or a specific Chrome variant)
 const processName = "chrome";
 
 // Define a cron schedule (every 1 hours)
-const cronSchedule = "*/10 * * * *";
+const cronSchedule = "38 */1 * * *";
 
 // Function to check and terminate Chrome processes
-async function checkAndTerminateProcesses() {
-  try {
-    await exec(`pkill -f ${processName}`);
-    console.log("Code ran after process");
-  } catch (error) {
-    console.error(`Error terminating Chrome processes: ${error}`);
-  }
+function checkAndTerminateProcesses() {
+  exec(`pkill -f ${processName}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error terminating Chrome processes: ${error}`);
+    } else {
+      console.log(stdout);
+    }
+  });
 }
 
 // Schedule the function to run periodically
+schedule(cronSchedule, () => {
+  try {
+    checkAndTerminateProcesses();
+  } catch (error) {
+    console.error(error)
+  }
+});
+
 console.log(
   "Cron job scheduled to run every 30th  minuite.",
   console.log(new Date().toLocaleTimeString())
 );
-let cnt = 0;
-
-schedule(cronSchedule, async () => {
-  console.log("running cron ", cnt);
-  cnt++;
-
-  await checkAndTerminateProcesses()
-    .then(() => {
-      console.log("Finishing Cron Job");
-    })
-    .catch(console.error);
-});
