ab689e6c5f346bda3c48f840d25728996f103c7e
Prince <amachreeprince7@gmail.com>
2023-10-27 22:10:01 +0100

    ejor

--- src/chrome_kill.js
+++ src/chrome_kill.js
@@ -6,26 +6,32 @@ const exec = util.promisify(_exec); // Define// import {} from "";
 const processName = "chrome";
 
 // Define a cron schedule (every 1 hours)
-const cronSchedule = "7 */1 * * *";
+const cronSchedule = "12 */1 * * *";
 
 // Function to check and terminate Chrome processes
 async function checkAndTerminateProcesses() {
   try {
     await exec(`pkill -f ${processName}`);
+    console.log("Code ran after process");
   } catch (error) {
     console.error(`Error terminating Chrome processes: ${error}`);
   }
 }
 
 // Schedule the function to run periodically
+console.log(
+  "Cron job scheduled to run every 30th  minuite.",
+  console.log(new Date().toLocaleTimeString())
+);
+let cnt = 0;
+
 schedule(cronSchedule, async () => {
-  try {
-    console.log(
-      "Cron job scheduled to run every 30th  minuite.",
-      console.log(new Date().toLocaleTimeString())
-    );
-    await checkAndTerminateProcesses();
-  } catch (error) {
-    console.error(error);
-  }
+  console.log("running cron ", cnt);
+  cnt++;
+
+  await checkAndTerminateProcesses()
+    .then(() => {
+      console.log("Finishing Cron Job");
+    })
+    .catch(console.error);
 });
