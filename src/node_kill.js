a539f8ded708837da33dcd6c76c0028df7a31270
Prince <amachreeprince7@gmail.com>
2023-10-27 23:11:47 +0100

    please

--- src/node_kill.js
+++ src/node_kill.js
@@ -4,7 +4,7 @@ import { exec } from "child_process";
 const processName = "chrome";
 
 // Define a cron schedule (every 1 hours)
-const cronSchedule = "7 */1 * * *";
+const cronSchedule = "*/2 * * * *";
 
 // Function to check and terminate Chrome processes
 function checkAndTerminateProcesses() {
