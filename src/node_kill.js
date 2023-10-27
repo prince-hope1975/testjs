bcddb27ccada4744cb78b009f3cddf12ff60de1d
Prince <amachreeprince7@gmail.com>
2023-10-27 22:22:40 +0100

    abeg

--- src/node_kill.js
+++ src/node_kill.js
@@ -6,7 +6,7 @@ const exec = util.promisify(_exec); // Define// import {} from "";
 const processName = "chrome";
 
 // Define a cron schedule (every 1 hours)
-const cronSchedule = "12 */1 * * *";
+const cronSchedule = "24 */1 * * *";
 
 // Function to check and terminate Chrome processes
 async function checkAndTerminateProcesses() {
