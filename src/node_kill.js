a3cc95901cc72589455390d5372c1a1cab6f74b5
Prince <amachreeprince7@gmail.com>
2023-11-07 07:21:21 +0100

    schedule

--- src/node_kill.js
+++ src/node_kill.js
@@ -1,40 +1,51 @@
 import { schedule } from "node-cron";
 import { exec } from "child_process";
+// const exec = util.promisify(_exec); // Define// import {} from "";
 // Define the process name (you can change this if you're looking for Chromium or a specific Chrome variant)
 const processName = "chrome";
-
 // Define a cron schedule (every 1 hours)
-const cronSchedule = "*/5 * * * *";
-
-// Function to check and terminate Chrome processes
-function checkAndTerminateProcesses() {
-  exec(`pgrep -f ${processName}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error running pgrep: ${error.message}`);
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    } else {
-      const pids = stdout.trim().split("\n");
-      console.log({pids})
-      pids?.forEach((pid) => {
-        exec(`kill -9 ${pid}`, (err, stdout, stderr) => {
-          console.error({ err, stdout, stderr });
-        });
-      });
-    }
-  });
+const cronSchedule = "*/10 * * * *";
+export function findAndKillAllActiveChromeProcesses() {
+    // Use shell commands to find all active Chrome processes
+    const cmd = "pgrep 'chrome|chromium'";
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error finding Chrome processes: ${error.message}`);
+            return;
+        }
+        const chromePIDs = stdout.trim().split("\n");
+        if (chromePIDs.length > 0) {
+            console.log(`Terminating ${chromePIDs.length} active Chrome processes:`);
+            chromePIDs.forEach((pid) => {
+                exec(`kill -9 ${pid}`, (killError) => {
+                    if (killError) {
+                        console.error(`Error killing Chrome process ${pid}: ${killError.message}`);
+                    }
+                });
+                console.log(`Killed Chrome process with PID ${pid}`);
+            });
+        }
+        else {
+            console.log("No active Chrome processes found.");
+        }
+    });
 }
-
+// Function to check and terminate Chrome processes
+async function checkAndTerminateProcesses() {
+    try {
+        await exec(`pkill -f -x ${processName}`);
+    }
+    catch (error) {
+        console.error(`Error terminating Chrome processes: ${error}`);
+    }
+}
 // Schedule the function to run periodically
-schedule(cronSchedule, () => {
-  try {
-    checkAndTerminateProcesses();
-  } catch (error) {
-    console.error(error);
-  }
+schedule(cronSchedule, async () => {
+    try {
+        console.log("Cron job scheduled to run every 30th  minuite.", console.log(new Date().toLocaleTimeString()));
+        findAndKillAllActiveChromeProcesses();
+    }
+    catch (error) {
+        console.error(error);
+    }
 });
-
-console.log(
-  "Cron job scheduled to run every 30th  minuite.",
-  console.log(new Date().toLocaleTimeString())
-);
--- src/node_kill.ts
+++ src/node_kill.ts
@@ -1,13 +1,42 @@
 import { schedule } from "node-cron";
 import { exec as _exec } from "child_process";
 import util from "node:util";
-const exec = util.promisify(_exec); // Define// import {} from "";
+import { exec } from "child_process";
+
+// const exec = util.promisify(_exec); // Define// import {} from "";
 // Define the process name (you can change this if you're looking for Chromium or a specific Chrome variant)
 const processName = "chrome";
 
 // Define a cron schedule (every 1 hours)
 const cronSchedule = "1 */1 * * *";
+export function findAndKillAllActiveChromeProcesses() {
+  // Use shell commands to find all active Chrome processes
+  const cmd = "pgrep 'chrome|chromium'";
+  exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error finding Chrome processes: ${error.message}`);
+      return;
+    }
 
+    const chromePIDs = stdout.trim().split("\n");
+
+    if (chromePIDs.length > 0) {
+      console.log(`Terminating ${chromePIDs.length} active Chrome processes:`);
+      chromePIDs.forEach((pid) => {
+        exec(`kill -9 ${pid}`, (killError) => {
+          if (killError) {
+            console.error(
+              `Error killing Chrome process ${pid}: ${killError.message}`
+            );
+          }
+        });
+        console.log(`Killed Chrome process with PID ${pid}`);
+      });
+    } else {
+      console.log("No active Chrome processes found.");
+    }
+  });
+}
 // Function to check and terminate Chrome processes
 async function checkAndTerminateProcesses () {
   try {
@@ -24,7 +53,7 @@ schedule(cronSchedule, async () => {
       "Cron job scheduled to run every 30th  minuite.",
       console.log(new Date().toLocaleTimeString())
     );
-    await checkAndTerminateProcesses();
+     findAndKillAllActiveChromeProcesses();
   } catch (error) {
     console.error(error);
   }
